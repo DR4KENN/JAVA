1a75e23db55f32024b0dce98010c5ffc97839262
DR4KENN <113258573+DR4KENN@users.noreply.github.com>
2024-02-09 12:36:12 +0100

    Add files via upload

---
+++ class/src/student.java
@@ -0,0 +1,30 @@
+public class student {
+    
+    private String nom;
+    private String prenom;
+    private int matricule;
+
+   
+    public student(String nom, String prenom, int matricule) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.matricule = matricule;
+    }
+
+    
+    public void insererNotes(float notes) {
+        
+        System.out.println("Notes insérées pour l'étudiant " + prenom + " " + nom);
+    }
+    
+    
+    public void supprimerNotes () {
+    	System.out.println("Notes supprimes pour l'etudiant" + prenom + " "+nom);
+    }
+    
+        
+       
+
+        
+       
+}
